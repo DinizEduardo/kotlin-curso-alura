91cae56d2f508a6b78dc4e6cafca79c9eca49fbe
DinizEduardo <eduardo.diniz987@gmail.com>
2021-03-18 15:03:41 -0300

    Criando getters e setters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,30 +3,21 @@ fun main() {
     val contaEduardo = Conta()
     contaEduardo.titular = "Eduardo"
     contaEduardo.numero = 7777
-    contaEduardo.saldo = 100.0
+    contaEduardo.setSaldo(100.0)
 
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1151
-    contaFran.saldo = 300.0
+    contaFran.setSaldo(200.0)
 
     println(contaEduardo.titular)
     println(contaEduardo.numero)
-    println(contaEduardo.saldo)
+    println(contaEduardo.getSaldo())
     println()
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.saldo)
-
-    println("transferencia da conta da fran para Eduardo")
-    if(contaFran.transfere(301.0, contaEduardo)) {
-        println("Transferiu!")
-        println("Saldo eduardo ${contaEduardo.saldo}")
-        println("Saldo fran ${contaFran.saldo}")
-    }else {
-        println("Falhou")
-    }
+    println(contaFran.getSaldo())
 
 }
 
@@ -34,7 +25,7 @@ fun main() {
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
     fun deposita(valor: Double) {
         this.saldo += valor
@@ -49,10 +40,19 @@ class Conta {
     fun transfere(valor: Double, destino: Conta): Boolean {
         if (this.saldo >= valor) {
             this.saldo -= valor
-            destino.saldo += valor
+            destino.deposita(valor)
             return true
         }
 
         return false
+    }
+
+    fun getSaldo(): Double {
+        return saldo;
+    }
+
+    fun setSaldo(valor: Double) {
+        if(valor > 0)
+            this.saldo = valor;
     }
 }
