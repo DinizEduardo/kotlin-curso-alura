02197b67ecd95f387868b848e008f83115d63eb4
DinizEduardo <eduardo.diniz987@gmail.com>
2021-03-18 16:02:30 -0300

    Criando a classe de Funcionario

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,16 @@
 fun main() {
     println("Bem vindo ao banco")
 
-//    testaComportamentosConta()
+    val eduardo = Funcionario(
+        nome = "Eduardo",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("Nome: ${eduardo.nome}")
+    println("CPF: ${eduardo.cpf}")
+    println("Salario: ${eduardo.salario}")
+    println("Bonificacao: ${eduardo.bonifcacao()}")
+
 }
 
