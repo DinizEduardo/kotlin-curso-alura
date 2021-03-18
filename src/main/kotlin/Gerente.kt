9157a58b29f235b5321f66466757ec85e37cefdc
DinizEduardo <eduardo.diniz987@gmail.com>
2021-03-18 16:26:24 -0300

    Usando herança

---
+++ src/main/kotlin/Gerente.kt
@@ -0,0 +1,16 @@
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double,
+    val senha: Int
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
+
+    fun autentica(senha: Int): Boolean {
+        return this.senha == senha
+    }
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,5 +12,38 @@ fun main() {
     println("Salario: ${eduardo.salario}")
     println("Bonificacao: ${eduardo.bonifcacao()}")
 
-}
+    val fran = Gerente(
+        nome = "Eduardo",
+        cpf = "111.111.111-11",
+        salario = 5000.0,
+        senha = 123456
+    )
 
+    println("Nome: ${eduardo.nome}")
+    println("CPF: ${eduardo.cpf}")
+    println("Salario: ${eduardo.salario}")
+    println("Bonificacao: ${eduardo.bonifcacao()}")
+
+    if(fran.autentica(123456)) {
+        println("Certo")
+    }else {
+        println("Falha")
+    }
+
+
+    val gui = Diretor(
+        nome = "Guilherme",
+        cpf = "111.111.111-11",
+        salario = 50000.0,
+        senha = 123456,
+        plr = 500.0
+    )
+
+    println("Nome: ${gui.nome}")
+    println("CPF: ${gui.cpf}")
+    println("Salario: ${gui.salario}")
+    println("Bonificacao: ${gui.bonifcacao()}")
+    println("PLR: ${gui.plr}")
+
+
+}
