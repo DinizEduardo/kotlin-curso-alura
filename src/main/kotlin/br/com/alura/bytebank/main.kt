4fbe8f8f74563ae67c15ee4883cc87aa8dfeec87
DinizEduardo <eduardo.diniz987@gmail.com>
2021-03-22 14:03:41 -0300

    Utilizando variaveis globais

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,15 @@
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.total
+
 fun main() {
-    testaComportamentosConta()
+
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1111)
+
+    testaContasDiferentes()
+
+    println("Total de contas: ${total}")
 }
 
 
--- src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,11 +1,18 @@
 package br.com.alura.bytebank.modelo
 
+var total = 0
+    private set
+
 abstract class Conta(
     val titular: Cliente,
     val numero: Int
 ) {
     var saldo = 0.0
         protected set
+
+    init {
+        total++
+    }
 
     fun deposita(valor: Double) {
         if(valor > 0)
